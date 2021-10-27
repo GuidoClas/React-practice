8f6b02f04947faa083dfb4aecf4a45f2bd30dcf8
Clas <A446089@santandertecnologia.com.ar>
2021-10-27 17:46:57 -0300

    UseState and react hooks practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import Expenses from './components/Expenses/Expenses';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
-    {title: "Car insurance", amount: 300, date: new Date()},
-    {title: "Electricity", amount: 256, date: new Date()},
-    {title: "Gas service", amount: 321, date: new Date()},
-    {title: "New laptop", amount: 444, date: new Date()}
+    { title: "Car insurance", amount: 300, date: new Date() },
+    { title: "Electricity", amount: 256, date: new Date() },
+    { title: "Gas service", amount: 321, date: new Date() },
+    { title: "New laptop", amount: 444, date: new Date() },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
-    <Expenses items={expenses}></Expenses>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+      <Expenses items={expenses}></Expenses>
+    </div>
   );
 }
 
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,25 @@
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const changeFilterHandler = (ev) => {
+    const filterData = ev.target.value;
+
+    props.onChangeFilter(filterData);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={changeFilterHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
